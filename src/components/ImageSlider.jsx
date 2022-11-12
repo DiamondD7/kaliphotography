a5c25557e4846b5aec186e16444f825661a86145
diamond <aaron.diamond2@yahoo.co.nz>
2022-11-13 01:04:19 +1300

    first comit on this repo

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { CaretRight, CaretLeft } from "phosphor-react";
+
+const ImageSlider = ({ imgs }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderStyle = {
+    position: "relative",
+    height: "100%",
+  };
+
+  const slideStyle = {
+    width: "100vw",
+    height: "980px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "0% 90%",
+    backgroundImage: `url(${imgs[currentIndex].url})`,
+    zIndex: "2",
+  };
+
+  const clickNext = () => {
+    const isLastSlide = currentIndex === imgs.length - 1;
+    const newInd = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newInd);
+  };
+
+  const clickPrev = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newInd = isFirstSlide ? imgs.length - 1 : currentIndex - 1;
+    setCurrentIndex(newInd);
+  };
+
+  const dotsClicked = (index) => {
+    setCurrentIndex(index);
+  };
+  return (
+    <div>
+      <button className="arr-right" onClick={clickNext}>
+        <CaretRight size={60} color={"#e2e2e2"} />
+      </button>
+      <button className="arr-left" onClick={clickPrev}>
+        <CaretLeft size={60} color={"#e2e2e2"} />
+      </button>
+      <div className="slide" style={slideStyle}></div>
+    </div>
+  );
+};
+
+export default ImageSlider;
