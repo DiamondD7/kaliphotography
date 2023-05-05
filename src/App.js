d8db5ede91f57cc7741372ba3978c7376b45c14b
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-06 01:36:02 +1200

    added the mail icon open and closing and the phone number hover effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Home from './components/Home';
-import { MapPin, Phone, InstagramLogo, EnvelopeOpen } from "phosphor-react";
+import { MapPin, Phone, InstagramLogo, Envelope, EnvelopeOpen } from "phosphor-react";
 import About from './components/About';
 import Gallery from './components/Gallery';
 import PreviewGallery from './components/Preview Gallery/previewGallery';
@@ -20,6 +20,34 @@ const App = () => {
         console.log("Error in te switch statement");
         break;
     }
+
+    useEffect(() => {
+      const contactNumber = document.getElementById('contactNumber');
+      const phoneIcon = document.getElementById('phoneIcon');
+
+      const envOpen = document.getElementById('envOpen');
+      const envClose = document.getElementById('envClose');
+      
+      phoneIcon.addEventListener('mouseenter', function() {
+        contactNumber.style.display = 'block';
+      })
+
+      phoneIcon.addEventListener('mouseleave', function() {
+        contactNumber.style.display = 'none';
+      })
+
+      envClose.addEventListener('mouseenter', function (){
+        envOpen.style.display = 'block';
+        envClose.style.display = 'none';
+        
+      })
+
+      envClose.addEventListener('mouseleave', function (){
+        envClose.style.display = 'block';
+        envOpen.style.display = 'none';
+      })
+
+    },[])
     return(
         <div>
           <div className='testtest'>
@@ -45,8 +73,11 @@ const App = () => {
               <button className="nav-btns">Book</button>
             </li>
           </ul>
+          <div className='phonenumber__wrapper' id='contactNumber'>
+            <p className='phonenumber__text'>+642102775992</p>
+          </div>
           <ul className="social-ul">
-            <li>
+            <li id='phoneIcon'>
               <Phone size={25} color={"#202020"} />
             </li>
             <li>
@@ -54,8 +85,11 @@ const App = () => {
                 <InstagramLogo size={25} color={"#202020"} />
               </a>
             </li>
-            <li>
+            <li className='envOpen-Icon' id='envOpen'>
               <EnvelopeOpen size={25} color={"#202020"} />
+            </li>
+            <li id='envClose'>
+              <Envelope size={25} color={"#202020"}/>
             </li>
             <li>
               <MapPin size={25} color={"#202020"} />
