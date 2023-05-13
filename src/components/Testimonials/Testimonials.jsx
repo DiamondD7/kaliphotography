732ebc0484187392841e0192dc722986b69edf49
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-14 00:43:01 +1200

    Added the name logo

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ErinWayne from "../../images/erinwayneimages/ew27.jpg";
+import ErinWayneNameLogo from "../../images/testimonialimages/erinwayne.png";
 
 import "../../styles/testimonialsstyles.css";
 
@@ -19,33 +20,40 @@ const Testimonials = () => {
               width: "700px",
             }}
           ></div>
-
-          <p className="erinwayne_statement">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi
-            maiores magni quibusdam, voluptatem ipsa pariatur, totam dolorem
-            odio quia obcaecati. Porro, animi? Similique fuga provident, qui
-            tenetur id odio! Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Expedita aut esse quasi hic repellat molestiae accusamus. Nemo
-            dicta reprehenderit vero saepe! Quia hic maxime magnam ipsam
-            consequuntur excepturi nobis qui? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Quam velit nemo ipsum, quasi officia
-            impedit, aliquam laboriosam perspiciatis molestiae rerum commodi
-            iusto ex voluptatibus fugit voluptates! Nulla dolores ipsa corporis.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis vel
-            explicabo, error delectus dolores adipisci laudantium! Rem aperiam
-            natus rerum adipisci animi, quod ipsum sequi sed, dicta dolores,
-            repellendus quae. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Tempora quibusdam explicabo accusantium voluptatum expedita
-            nisi sed! Eaque perspiciatis nihil quae ducimus inventore cum velit,
-            dolore nemo quas praesentium, at libero. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Tenetur corporis modi vel provident
-            nisi libero inventore alias quas explicabo earum praesentium
-            repellendus quos, dolorum consequuntur atque exercitationem?
-            Temporibus, minus rem. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Reprehenderit sint quibusdam quae fugiat natus
-            nam. Maxime deleniti, architecto fugiat iusto sint molestias dicta
-            possimus eum dolore, numquam quas culpa reprehenderit.
-          </p>
+          <div>
+            <p className="erinwayne_statement">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              quasi maiores magni quibusdam, voluptatem ipsa pariatur, totam
+              dolorem odio quia obcaecati. Porro, animi? Similique fuga
+              provident, qui tenetur id odio! Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Expedita aut esse quasi hic repellat
+              molestiae accusamus. Nemo dicta reprehenderit vero saepe! Quia hic
+              maxime magnam ipsam consequuntur excepturi nobis qui? Lorem ipsum
+              dolor, sit amet consectetur adipisicing elit. Quam velit nemo
+              ipsum, quasi officia impedit, aliquam laboriosam perspiciatis
+              molestiae rerum commodi iusto ex voluptatibus fugit voluptates!
+              Nulla dolores ipsa corporis. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Quis vel explicabo, error delectus
+              dolores adipisci laudantium! Rem aperiam natus rerum adipisci
+              animi, quod ipsum sequi sed, dicta dolores, repellendus quae.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              quibusdam explicabo accusantium voluptatum expedita nisi sed!
+              Eaque perspiciatis nihil quae ducimus inventore cum velit, dolore
+              nemo quas praesentium, at libero. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Tenetur corporis modi vel provident
+              nisi libero inventore alias quas explicabo earum praesentium
+              repellendus quos, dolorum consequuntur atque exercitationem?
+              Temporibus, minus rem. Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Reprehenderit sint quibusdam quae fugiat natus
+              nam. Maxime deleniti, architecto fugiat iusto sint molestias dicta
+              possimus eum dolore, numquam quas culpa reprehenderit.
+            </p>
+            <img
+              className="nameslogo"
+              src={ErinWayneNameLogo}
+              alt="erinwaynenames"
+            />
+          </div>
         </div>
       </div>
     </div>
