baaa6726777ba6a016c41d2e7e5d5ac67d42c282
diamond <aaron.diamond2@yahoo.co.nz>
2023-08-24 02:37:38 +1200

    updated some feature and pictures based on client's

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,145 +1,32 @@
 import React, { useState } from "react";
 import AboutTitle from "../images/aboutimages/AboutTitle.png";
+import KaliAbout from "../images/aboutimages/KaliPicAboutMe.jpg";
+
+import "../styles/aboutmestyles.css";
 const About = () => {
   const [historyActive, setHistoryActive] = useState(true);
   const [passionActive, setPassionActive] = useState(false);
   const [careerActive, setCareerActive] = useState(false);
   const [gearsActive, setGearsActive] = useState(false);
-
-  // const historyClicked = () => {
-  //   setHistoryActive(true);
-  //   setPassionActive(false);
-  //   if (historyActive === true) {
-  //     document.getElementById("history").classList.add("active");
-  //     document.getElementById("passion").classList.remove("active");
-  //   }
-  // };
-
-  // const passionClicked = () => {
-  //   setPassionActive(true);
-  //   setHistoryActive(false);
-
-  //   if (passionActive === true) {
-  //     document.getElementById("history").classList.remove("active");
-  //     document.getElementById("passion").classList.add("active");
-  //   }
-  // };
   return (
     <div>
-      {/* <h1 className="introduction-title">This is about me</h1> */}
       <img className="introduction-title" src={AboutTitle} />
+      <img
+        className="kali-pic-aboutme"
+        src={KaliAbout}
+        alt="kaliaboutmepicture"
+      />
       <div className="border-line"></div>
-      {historyActive === true ? (
-        <p className="introduction-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          necessitatibus similique, nam sunt, eaque nihil fugit dolore a cumque
-          vitae ipsam sit reiciendis? Eius, impedit! Obcaecati veritatis
-          praesentium sapiente aliquid? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Id quae earum, dolorem quo voluptas reiciendis culpa
-          a eaque et nobis, mollitia fugiat ipsa itaque, numquam neque dicta ex
-          officiis? Architecto. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Atque exercitationem maxime tempore, nihil illo quas
-          eaque nobis animi vel debitis itaque ipsam nam veniam provident
-          voluptate repellat voluptatibus optio necessitatibus! Lorem ipsum,
-          dolor sit amet consectetur adipisicing elit. Sed, veritatis veniam!
-          Officiis illo neque voluptatibus ducimus amet animi voluptate
-          reiciendis consequatur deserunt dolor, eius quibusdam, asperiores
-          inventore quas? Molestias, quos.
-        </p>
-      ) : (
-        ""
-      )}
-
-      {passionActive === true ? (
-        <p className="introduction-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          necessitatibus similique, nam sunt, eaque nihil fugit dolore a cumque
-          vitae ipsam sit reiciendis? Eius, impedit! Obcaecati veritatis
-          praesentium sapiente aliquid? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Id quae earum, dolorem quo voluptas reiciendis culpa
-          a eaque et nobis, mollitia fugiat ipsa itaque, numquam neque dicta ex
-          officiis? Architecto. Lorem ipsum dolor sit amet, consectetur
-        </p>
-      ) : (
-        ""
-      )}
-
-      {careerActive === true ? (
-        <p className="introduction-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          necessitatibus similique, nam sunt, eaque nihil fugit dolore a cumque
-          vitae ipsam sit reiciendis? Eius, impedit! Obcaecati veritatis
-        </p>
-      ) : (
-        ""
-      )}
-
-      {gearsActive === true ? (
-        <p className="introduction-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-      ) : (
-        ""
-      )}
-
-      <div>
-        <ul className="about-ul">
-          <li>
-            <button
-              className={historyActive === true ? "active" : ""}
-              id="history"
-              onClick={() => {
-                setPassionActive(false);
-                setHistoryActive(true);
-                setCareerActive(false);
-                setGearsActive(false);
-              }}
-            >
-              History
-            </button>
-          </li>
-          <li>
-            <button
-              className={passionActive === true ? "active" : ""}
-              id="passion"
-              onClick={() => {
-                setPassionActive(true);
-                setHistoryActive(false);
-                setCareerActive(false);
-                setGearsActive(false);
-              }}
-            >
-              Passion
-            </button>
-          </li>
-          <li>
-            <button
-              className={careerActive === true ? "active" : ""}
-              onClick={() => {
-                setPassionActive(false);
-                setHistoryActive(false);
-                setCareerActive(true);
-                setGearsActive(false);
-              }}
-            >
-              Career
-            </button>
-          </li>
-          <li>
-            <button
-              className={gearsActive === true ? "active" : ""}
-              onClick={() => {
-                setPassionActive(false);
-                setHistoryActive(false);
-                setCareerActive(false);
-                setGearsActive(true);
-              }}
-            >
-              Gears
-            </button>
-          </li>
-        </ul>
-      </div>
+      <p className="introduction-paragraph">
+        Welcome to my photography realm, where I transform fleeting moments into
+        everlasting memories. I take great pleasure in capturing and preserving
+        your cherished memories in a tangible way. While moments may be
+        short-lived, the memories they create last forever. Through my
+        photography, I don't just snap pictures; I weave narratives — tales of
+        love, laughter, and the individual magic that each moment holds. Join me
+        on this adventure of turning the brief into the timeless, one click at a
+        time.
+      </p>
     </div>
   );
 };
